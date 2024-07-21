1041c61d9276a1d58cf71a30e14d8c6b0ee4bf84
desenvolve-introducao-web <zuisje0@gmail.com>
2024-07-20 23:14:00 -0300

    Add files via upload

---
+++ Módulo 2 - Fundamentos/2.2 - Tipos de dados/aula2_questao1.py
@@ -0,0 +1,7 @@
+uma_variável = 5
+outra_variável = 2
+uma_terceira_variável = uma_variável / outra_variável
+print("O resultado será:")
+print(uma_terceira_variável)
+print("Que é uma variável do tipo:")
+print(type(uma_terceira_variável))
---
+++ Módulo 2 - Fundamentos/2.2 - Tipos de dados/aula2_questao2.py
@@ -0,0 +1,20 @@
+uma_variável = "o resultado é"
+outra_variável = 10
+terceira_variável = 3.5
+outra_variável2 = outra_variável + terceira_variável
+print("A primeira variável criada tem o valor:")
+print(uma_variável)
+print("E a primeira variável é do tipo:")
+print(type(uma_variável))
+print("A segunda variável criada tem o valor:")
+print(outra_variável)
+print("E a segunda variável é do tipo:")
+print(type(outra_variável))
+print("A terceira variável criada tem o valor:")
+print(terceira_variável)
+print("E a terceira variável é do tipo:")
+print(type(terceira_variável))
+print("A quarta variável criada tem o valor:")
+print(outra_variável2)
+print("E a quarta variável é do tipo:")
+print(type(outra_variável2))
