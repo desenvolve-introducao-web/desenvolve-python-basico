268afa1426fd6a64c37b507ae44f3fd5cfdd2866
desenvolve-introducao-web <zuisje0@gmail.com>
2024-07-22 16:56:27 -0300

    Add files via upload

--- Módulo 2 - Fundamentos/2.4 - input e print/aula4_questao1.py
+++ Módulo 2 - Fundamentos/2.4 - input e print/aula4_questao1.py
@@ -1,5 +1,6 @@
-comprimento = int(input("Digíte o comprimento do terreno em metros: "))
-largura = int(input("Digíte a largura do terreno em metros: "))
+#perguntando qual o comprimento do terreno, transformando a resposta em float, depois armazenando na variável comprimento
+comprimento = float(input("Digíte o comprimento do terreno em metros: "))
+largura = float(input("Digíte a largura do terreno em metros: "))
 preço_m2 = float(input("Digíte o preço do metro quadrado da região: "))
 area = comprimento * largura
 preço = area * preço_m2
