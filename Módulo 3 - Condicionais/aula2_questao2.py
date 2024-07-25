43a644f614966c64d6420b9f63419a5a4e6478d6
desenvolve-introducao-web <zuisje0@gmail.com>
2024-07-25 12:43:49 -0300

    Delete Módulo 3 - Condicionais/aula2_questao2.py

--- Módulo 3 - Condicionais/aula2_questao2.py
+++
@@ -1,4 +0,0 @@
-print("Juliana e Cris querem entrar em um bar, mas só podem se ambos forem maiores de idade")
-juliana = int(input("Insira a idade de Juliana: "))
-cris = int(input("Insira a idade de Cris: "))
-print("Juliana e Cris podem entrar no bar?", (juliana >= 18) or (cris >= 18))
